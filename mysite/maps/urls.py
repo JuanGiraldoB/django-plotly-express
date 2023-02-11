19fbda611121eba79100daaa77e5b2640a08886c
JuanGiraldoB <jugiraldob@unal.edu.co>
2023-02-11 11:15:55 -0500

    added selection points graph and view

--- mysite/maps/urls.py
+++ mysite/maps/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.chart, name='chart')
+    # path('', views.chart, name='chart')
+    path('', views.audio, name='audio')
 ]
--- mysite/maps/views.py
+++ mysite/maps/views.py
@@ -3,14 +3,16 @@ import plotly.express as px
 
 
 def chart(request):
-    df = px.data.carshare()
-
-    fig = px.scatter_mapbox(df, lat="centroid_lat", lon="centroid_lon", color="peak_hour", size="car_hours",
-                            color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10,
-                            mapbox_style="carto-positron")
+    df = px.data.iris()
+    fig = px.density_heatmap(
+        df, x="sepal_width", y="sepal_length", marginal_x="rug", marginal_y="histogram")
 
     chart = fig.to_html()
 
     context = {'chart': chart}
 
     return render(request, 'maps/chart.html', context)
+
+
+def audio(request):
+    return render(request, 'maps/audio.html')
